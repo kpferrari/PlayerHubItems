7e5d09a8bb76314d09a2eada130bd56f5219674b
Bodsterrr <jacob.boddey@hotmail.co.uk>
2015-08-10 13:01:04 +0100

    Work In Progress

--- src/com/Bodsterrr/PlayerHubItems/Book.java
+++ src/com/Bodsterrr/PlayerHubItems/Book.java
@@ -1,5 +1,8 @@
 package com.Bodsterrr.PlayerHubItems;
 
+import java.io.IOException;
+
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
@@ -8,14 +11,32 @@ public class Book {
 	
 	public static void giveBook(Player p) {
 		
-		String BookName = Config.bookCfg.getString("Name");
-		
+		String BookName = Config.bookCfg.getString("Book.meta.title");
+		String BookAuthor = Config.bookCfg.getString("Book.meta.author");
 		ItemStack Book = Config.bookCfg.getItemStack("Book");
 		BookMeta BookM = (BookMeta) Book.getItemMeta();
 		for (int i=0;i<=BookM.getPageCount();i++) {
 			BookM.setPage(i, BookM.getPage(i).replaceAll("&", "§").replaceAll("%player%", p.getName()));
 		}
 		BookM.setDisplayName(BookName.replaceAll("&", "§"));
+		BookM.setAuthor(BookAuthor.replaceAll("&", "§"));
+	}
+	
+	public static void saveBook(Player p) {
+		
+		if (p.getItemInHand() != null && p.getItemInHand().getType() == Material.WRITTEN_BOOK) {
+			Config.bookCfg.set("Book", p.getItemInHand());
+			try {
+				Config.bookCfg.save(Config.bookFile);
+			} catch (IOException e) {
+				
+			}
+			
+			p.sendMessage("§7# §3The book in your hand has been saved to §dbook.yml");
+		}
+		else {
+			p.sendMessage("§7# §cThat is not a written book");
+		}
 		
 	}
 
--- src/com/Bodsterrr/PlayerHubItems/Commands.java
+++ src/com/Bodsterrr/PlayerHubItems/Commands.java
@@ -3,15 +3,60 @@ package com.Bodsterrr.PlayerHubItems;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
+		//Make messages customisable
+		
 		if (args.length == 0) {
 			
-			// (/playerhubitems | /phi)
+			if (sender.hasPermission("playerhubitems.command") || sender.isOp()) {
+				sender.sendMessage("§7# §3Use §7(§d/phi head§7) §3for head commands");
+				sender.sendMessage("§7# §3Use §7(§d/phi book§7) §3for book commands");
+			}
+			else {
+				sender.sendMessage("§7# §3This server is using §dPlayerHubItems");
+				sender.sendMessage("§7# §3Using version §d2.5 §3by §6Bodsterr");
+			}
+			
+		}
+		
+		else if (args.length == 1) {
+			
+			if (args[0].equalsIgnoreCase("head")) {
+				sender.sendMessage("§7# §3Use §7(§d/phi head get§7) §3to get your head");
+			}
+			else if (args[0].equalsIgnoreCase("book")) {
+				sender.sendMessage("§7# §3Use §7(§d/phi book get§7) §3to get the book");
+				sender.sendMessage("§7# §3Use §7(§d/phi book save§7) §3to save the book");
+			}
+			
+		}
+		
+		else if (args.length == 2) {
+			if (args[0].equalsIgnoreCase("head")) {
+				if (args[1].equalsIgnoreCase("get")) {
+					Player player = (Player) sender;
+					Head.giveHead(player);
+					sender.sendMessage("§7# §3You have been given your head");
+				}
+			}
+			else if (args[0].equalsIgnoreCase("book")) {
+				if (args[1].equalsIgnoreCase("get")) {
+					Player player = (Player) sender;
+					Book.giveBook(player);
+					sender.sendMessage("§7# §3You have been give the book");
+				}
+				else if (args[1].equalsIgnoreCase("save")) {
+					Player player = (Player) sender;
+					Book.saveBook(player);
+				}
+				
+			}
 			
 		}
 		
